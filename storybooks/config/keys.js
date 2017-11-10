e3a509b5b8ac7e1db85064dd078ac2ac81f28505
ph2150105 <ph2150105@transcosmos-ph.local>
2017-11-10 16:52:30 +0800

    Added StoryBooks

---
+++ storybooks/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://jegger:admin@ds255455.mlab.com:55455/storybooks-dev',
+  googleClientID: '891608171914-cs133hgp0gm0e3kf919t6t9l43v7t6q7.apps.googleusercontent.com',
+  googleClientSecret: 'mEyoCpPLnrbASB6tt4at_QF0'
+}
